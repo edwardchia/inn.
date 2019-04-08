78287e793dc2f0a470dabb5f2787eec81894ec54
Edward Chia <chiaedward1@gmail.com>
2019-04-08 15:49:15 -0700

    added devise gem, simple form gem, bulma

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :first_name, :last_name, :phone_number, presence: true
 end
